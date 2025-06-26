d25a68af80d8a5567b6587358d101043ea9201f4
Aladyn1962 <mareknowak1238@gmail.com>
2025-06-26 10:38:52 +0200

    Implemented Gabinet class and basic operations

--- main.py
+++ main.py
@@ -24,3 +24,28 @@ map_widget = tkintermapview.TkinterMapView(ramka_mapa, width=1200, height=600, c
 map_widget.set_position(52.23, 21.0)  # Warsaw coordinates
 map_widget.set_zoom(6)
 map_widget.grid(row=0, column=0, columnspan=3)
+
+
+class Gabinet:
+    def __init__(self, nazwa, ulica, miejscowosc, nr_budynku):
+        self.nazwa = nazwa
+        self.ulica = ulica
+        self.miejscowosc = miejscowosc
+        self.nr_budynku = nr_budynku
+        self.pracownicy = []
+        self.pacjenci = []
+
+
+# main.py
+gabinet_list = []
+
+
+def dodaj_gabinet():
+    nazwa = entry_nazwa.get()
+    ulica = entry_ulica.get()
+    miejscowosc = entry_miejscowosc.get()
+    nr = entry_nr_budynku.get()
+
+    gabinet = Gabinet(nazwa, ulica, miejscowosc, nr)
+    gabinet_list.append(gabinet)
+    pokaz_liste_gabinetow()
